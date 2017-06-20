3cec708dbff14aaec038fbff81594c9c17aa18b9
张梦林 <dream_zml@163.com>
2017-06-20 21:28:33 +0800

    更新composer,加入空的module

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace dreamzml\SimpleGitPublish;
+
+/**
+ * simplegitpublish module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'dreamzml\SimpleGitPublish\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
